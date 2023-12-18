cec44afff51cc4aeebcc0079d395f62206eeb91c
lichenhui <lichenhui821@qq.com>
2023-12-19 00:16:42 +0800

    feat: 初始化 user 表

---
+++ model/main/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"gorm.io/gorm/schema"
+	"log"
+	"os"
+	"time"
+
+	"go-servers/model"
+)
+
+func main() {
+	dsn := "root:nestjs-exercise@tcp(127.0.0.1:3306)/go_exercise_user_srv?charset=utf8mb4&parseTime=True&loc=Local"
+
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags),
+		logger.Config{
+			SlowThreshold: time.Second, // 慢 SQL 阀值
+			LogLevel:      logger.Info, // Log level
+			Colorful:      true,        // 禁用彩色打印
+		},
+	)
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		NamingStrategy: schema.NamingStrategy{
+			TablePrefix:   "",
+			SingularTable: true,
+			NameReplacer:  nil,
+		},
+		Logger: newLogger,
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	_ = db.AutoMigrate(&model.User{})
+}
